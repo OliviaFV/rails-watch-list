9ed3f537cb46d326caec7211d0bffaa285277f3d
Olivia Fierro <rovi_oli@hotmail.com>
2023-03-09 16:15:02 -0600

    movies

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    #  @bookmark = Bookmark.new
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+    redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+
+
+  def update
+    @list.update(list_params)
+      redirect_to @list, notice: 'List was successfully updated.'
+  end
+
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
